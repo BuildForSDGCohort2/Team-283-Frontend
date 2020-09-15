8b85cdf2cd78680229b1de559170a6d531066ba2
Aduda-Boaz <adudaboaz@gmail.com>
2020-09-15 19:38:22 +0300

    initial set-up

---
+++ farm-mall/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Connecting farmers to the ready consumers!!</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ farm-mall/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <p>This is the header section</p>
+        </div>
+    )
+}
+
+export default Header;
