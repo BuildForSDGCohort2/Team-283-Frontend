6964d325e4deb49b7ad6b6efc99051a2475ed880
Aduda-Boaz <adudaboaz@gmail.com>
2020-09-16 08:34:30 +0300

    initial app setup, header and navbar

---
+++ farm-mall/src/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <h2>This is my navigation bar</h2>
+        </div> 
+    )
+}
+
+export default NavBar
