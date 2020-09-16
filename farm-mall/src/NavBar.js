08e4dbe95b603b72756e580ddd4b96d19b1a6c2a
Aduda-Boaz <adudaboaz@gmail.com>
2020-09-16 10:06:23 +0300

    navbar modified

--- farm-mall/src/NavBar.js
+++ farm-mall/src/NavBar.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './NavBar.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function NavBar() {
     return (
         <div className="navbar">
-            <h2>This is my navigation bar</h2>
+            <div className="logo_icon">
+                <h2>FarmMall</h2>
+            </div>
+            <div className="navlinks">
+                <h2>Home</h2>
+                <h2>About Us</h2>
+                <h2>Shop</h2>
+                <h2>News</h2>
+                <h2>Contact Us</h2>
+            </div>
+            <div className="search_bar">
+                <form>
+                    <input type="text" placeholder="Search..." />
+                    <p>
+                    <input type="checkbox" />
+                    {' '}
+                    Only show products in stock
+                    </p>
+                </form>
+            </div>
         </div> 
     )
 }
