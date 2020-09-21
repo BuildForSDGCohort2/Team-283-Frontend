288dd984b06a192bd852bd6820d172d34f1fc2ec
Aduda-Boaz <adudaboaz@gmail.com>
2020-09-21 19:09:23 +0300

    adding the welcoming to the app message

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import NavBar from './NavBar';
+import Welcome from './Welcome';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NavBar />
+      <Welcome />
     </div>
   );
 }
